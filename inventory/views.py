a2c172b7de48965a3959e1ae5d02633f3e1ceeb3
ngoakor12 <ngoakor12@gmail.com>
2023-07-12 06:36:31 +0200

    ingredients list inventory view

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+
+from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
+
 
 # Create your views here.
+class IngredientListView(ListView):
+    model = Ingredient
+    template_name = "inventory/ingredient_list.html"
+    context_object_name="ingredients"
